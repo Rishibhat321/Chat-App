8ec1fb5df612c616e43bbbd49d191e3d43d2c23a
Rishib#321 <rishitab2209@gmail.com>
2025-03-02 20:23:33 +0530

    add configs and controllers

--- src/main/java/com/chat/app/controller/ChatController.java
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -1,4 +1,13 @@
 package com.chat.app.controller;
 
+import com.chat.app.model.ChatMessage;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
+
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+
 }
