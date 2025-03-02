d8d9676e361a2a6e38214c2f77f1e2e29a0310f7
Rishib#321 <rishitab2209@gmail.com>
2025-03-02 20:25:12 +0530

    add configs and controllers and chat model

--- src/main/java/com/chat/app/controller/ChatController.java
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -1,11 +1,15 @@
 package com.chat.app.controller;
 
 import com.chat.app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage message) {
         return message;
     }
